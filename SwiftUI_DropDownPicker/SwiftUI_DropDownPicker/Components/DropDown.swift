888e39017215092a101a83be40093ef5fe7ece9f
전지훈 <junji636@gmail.com>
2023-01-15 23:58:31 +0900

    dropdown 뷰 작업중

--- SwiftUI_DropDownPicker/SwiftUI_DropDownPicker/Components/DropDown.swift
+++ SwiftUI_DropDownPicker/SwiftUI_DropDownPicker/Components/DropDown.swift
@@ -16,7 +16,18 @@ struct DropDown: View {
     var body: some View {
         GeometryReader {
             let size = $0.size
+            
+            VStack(spacing: 0) {
+                ForEach(content) { title in
+                    RowView(title, size)
+                }
+            }
         }
         .frame(height: 55)
+    }
+    
+    @ViewBuilder
+    func RowView(_ title: String, _ size: CGSize) -> some View {
+        
     }
 }
